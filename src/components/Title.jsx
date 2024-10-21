9f0ac5e51d05e5c9d7f1fea874b13d5004d2feba
MauserWeb <173951485+wilder78@users.noreply.github.com>
2024-10-21 10:24:03 -0500

    Inicio de programacion navbar

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <a className="navbar-brand" href="./index.html">
+      <img src="../public/assets/image/LogoMs2.png" width="40" alt="LogoMs2" />
+      MMS-Repuestos
+    </a>
+  );
+}
+
+export default Title;
