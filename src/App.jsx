9aafde29c324d8cd1fcc0cbfb0698804b19c0172
MauserWeb <173951485+wilder78@users.noreply.github.com>
2024-10-22 09:44:27 -0500

    Creacion de las carpetas carrusel y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Carousel from "./components/carousel";
 
 function App() {
   return (
     <>
       <Header />
+      <Carousel />
+      <Footer />
     </>
 
   )
