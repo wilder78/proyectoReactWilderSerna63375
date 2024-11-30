aa34e8169c0f0c333ce6936e72b9eb99ae10a5b9
wilder78 <173951485+wilder78@users.noreply.github.com>
2024-11-30 15:37:22 -0500

    Avanced in the proyect --//-- avance en el proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import ItemListConteiner from "./components/ItemListConteiner/ItemListConteiner";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <ItemListConteiner />
     </>
 
---
+++ src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,9 @@
+const CardWidget = () => {
+  return (
+    <a className="nav-link" href="#">
+      <i className="bi bi-cart3"/>
+    </a>
+  );
+};
+
+export default CardWidget;
