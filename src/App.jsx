a1021d04ca4a745402b230a2be88e0476723df59
wilder78 <173951485+wilder78@users.noreply.github.com>
2024-12-04 16:30:19 -0500

    Modificación de los productos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import ItemListConteiner from "./components/ItemListConteiner/ItemListConteiner";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  useEffect(() => {
+    console.log("Anda a buscar los productos.")
+  },[])
+
   return (
     <>
       <Navbar />
